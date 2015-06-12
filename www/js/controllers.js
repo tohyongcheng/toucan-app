ad2831dc007e3fba9be76f5cfd4527c41185c0cf
Toh Yong Cheng <tohyongcheng@gmail.com>
2015-06-12 10:59:05 +0800

    add some code for camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -106,24 +106,23 @@ angular.module('starter.controllers', [])
 })
 
 .controller('SetupAccountCtrl', function($scope, $auth, $state, $rootScope, $cordovaCamera, $ionicPlatform) {
+  var options = {
+    quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL,
+    sourceType: Camera.PictureSourceType.CAMERA,
+    allowEdit: false,
+    encodingType: Camera.EncodingType.JPEG,
+    targetWidth: 100,
+    targetHeight: 100,
+    popoverOptions: CameraPopoverOptions,
+    saveToPhotoAlbum: false
+  };
+
   $scope.$on('$ionicView.beforeEnter', function() {
     $scope.parentForm = {};
-    
   }); 
 
   $ionicPlatform.ready(function() {
-    var options = {
-      quality: 50,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.CAMERA,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 100,
-      targetHeight: 100,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: false
-    };
-
     $scope.takephoto = function() {
       $cordovaCamera.getPicture(options).then(function(imageData) {
         var image = document.getElementById('myImage');
